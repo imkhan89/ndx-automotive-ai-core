1543f8c046c4b61ddbef393ce10476c103f84253
imkhan89 <info@ndestore.com>
2026-03-21 01:13:26 +0500

    Create ndx_parts.data.js

---
+++ ndx_data/ndx_data/ndx_parts.data.js
@@ -0,0 +1,15 @@
+export const NDX_PARTS = [
+  {
+    name: "Control Arm",
+    category: "Suspension",
+    positions: ["front", "rear", "left", "right", "lower", "upper"]
+  },
+  {
+    name: "Brake Pad",
+    category: "Braking"
+  },
+  {
+    name: "Air Filter",
+    category: "Engine"
+  }
+];
