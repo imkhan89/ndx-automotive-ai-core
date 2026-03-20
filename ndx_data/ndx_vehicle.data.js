367d9f7cc03dbff50f151364f8bb594bb3c1ee87
imkhan89 <info@ndestore.com>
2026-03-21 01:12:56 +0500

    Create ndx_vehicle.data.js

---
+++ ndx_data/ndx_vehicle.data.js
@@ -0,0 +1,22 @@
+export const NDX_VEHICLES = [
+  {
+    make: "Suzuki",
+    models: [
+      {
+        name: "Swift",
+        years: [2017, 2018, 2019, 2020, 2021],
+        engines: ["1.3L", "1300cc"]
+      }
+    ]
+  },
+  {
+    make: "Honda",
+    models: [
+      {
+        name: "Civic",
+        years: [2012, 2013, 2014, 2015, 2016],
+        engines: ["1.8L", "1800cc"]
+      }
+    ]
+  }
+];
