c8e8894cc0116829d219c0a3e845ce8bfce5554c
imkhan89 <info@ndestore.com>
2026-03-21 01:34:26 +0500

    Create orderCreation.js

---
+++ flows/autoParts/orderCreation.js
@@ -0,0 +1,13 @@
+const shopify = require("../../services/shopifyOrderService");
+const stateRepo = require("../../state/stateRepository");
+
+module.exports = async (user, text, state) => {
+
+  await shopify.createOrder(state);
+
+  state.step = "success";
+  stateRepo.set(user, state);
+
+  const next = require("./orderSuccess");
+  return next(user, "", state);
+};
