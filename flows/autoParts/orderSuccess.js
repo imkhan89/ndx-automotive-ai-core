60acb163c7c35db7ababdbc96f38cccdbdce14fe
imkhan89 <info@ndestore.com>
2026-03-22 00:23:59 +0500

    Update orderSuccess.js

--- flows/autoParts/orderSuccess.js
+++ flows/autoParts/orderSuccess.js
@@ -1,9 +1,11 @@
 const send = require("../../services/whatsappService").send;
 const stateRepo = require("../../state/stateRepository");
 
+const successTemplate = require("../../interface/templates/successTemplate");
+
 module.exports = (user, text, state) => {
 
   stateRepo.set(user, { flow: "main" });
 
-  return send(user, "Order placed successfully ✅");
+  return send(user, successTemplate());
 };
