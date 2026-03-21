fcb3f4875fbe0982279645312a897a95f183507f
imkhan89 <info@ndestore.com>
2026-03-22 00:23:19 +0500

    Update orderConfirmation.js

--- flows/autoParts/orderConfirmation.js
+++ flows/autoParts/orderConfirmation.js
@@ -1,14 +1,17 @@
 const send = require("../../services/whatsappService").send;
 const stateRepo = require("../../state/stateRepository");
 
+const input = require("../../interface/templates/inputTemplate");
+const error = require("../../interface/templates/errorTemplate");
+
 module.exports = (user, text, state) => {
 
-  if (text.toLowerCase() !== "yes") {
-    return send(user, "Order cancelled.");
+  if (!text || text.toLowerCase() !== "yes") {
+    return send(user, error.cancelled());
   }
 
   state.step = "customer";
   stateRepo.set(user, state);
 
-  return send(user, "Enter Your Name:");
+  return send(user, input.name());
 };
