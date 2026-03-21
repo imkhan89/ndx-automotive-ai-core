4e44a6ccf36aec3809624e18562a096221388aef
imkhan89 <info@ndestore.com>
2026-03-21 09:12:06 +0500

    Create productMap.js

---
+++ utils/productMap.js
@@ -0,0 +1,39 @@
+const productMap = [
+  {
+    keywords: ["corolla", "toyota corolla"],
+    products: [
+      {
+        name: "Denso Air Filter Corolla 2008-2026",
+        keywords: ["air filter", "filter"],
+        link: "https://ndestore.com/products/denso-air-filter-corolla-2008-2026",
+        price: "PKR 2,500",
+      },
+      {
+        name: "Brake Pads Corolla",
+        keywords: ["brake", "brake pads"],
+        link: "https://ndestore.com/products/corolla-brake-pads",
+        price: "PKR 4,500",
+      },
+    ],
+  },
+
+  {
+    keywords: ["civic", "honda civic"],
+    products: [
+      {
+        name: "Air Filter Honda Civic 2016-2021",
+        keywords: ["air filter", "filter"],
+        link: "https://ndestore.com/products/civic-air-filter",
+        price: "PKR 2,800",
+      },
+      {
+        name: "Brake Pads Civic",
+        keywords: ["brake", "brake pads"],
+        link: "https://ndestore.com/products/civic-brake-pads",
+        price: "PKR 5,000",
+      },
+    ],
+  },
+];
+
+module.exports = productMap;
