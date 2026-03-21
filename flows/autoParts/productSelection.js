9a72c8e6479bdd0303b82f8dc64a2129fcff2c8b
imkhan89 <info@ndestore.com>
2026-03-21 23:47:37 +0500

    Update productSelection.js

--- flows/autoParts/productSelection.js
+++ flows/autoParts/productSelection.js
@@ -1,24 +1,24 @@
 const send = require("../../services/whatsappService").send;
 const stateRepo = require("../../state/stateRepository");
 
+const validate = require("../../decisions/validationDecision");
+const confirm = require("../../interface/templates/confirmationTemplate");
+const error = require("../../interface/templates/errorTemplate");
+const pricing = require("../../decisions/pricingDecision");
+
 module.exports = (user, text, state) => {
 
-  const product = state.products[text - 1];
+  const valid = validate(text, state.products.length);
 
-  if (!product) {
-    return send(user, "Invalid selection. Try again.");
-  }
+  if (!valid) return send(user, error.invalidSelection());
+
+  let product = state.products[text - 1];
+
+  product = pricing(user, product);
 
   state.selected = product;
   state.step = "confirm";
   stateRepo.set(user, state);
 
-  return send(user,
-`Confirm Order:
-
-${product.title}
-${product.price}
-
-Reply YES to confirm`
-  );
+  return send(user, confirm(product));
 };
