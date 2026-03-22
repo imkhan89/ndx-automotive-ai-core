a421c31a51afe24b4bc3847ce0e0af98e9ed9beb
imkhan89 <info@ndestore.com>
2026-03-22 19:54:20 +0500

    Update makeSelection.js

--- flows/autoParts/makeSelection.js
+++ flows/autoParts/makeSelection.js
@@ -1,5 +1,6 @@
 const send = require("../../services/whatsappService").send;
 const stateRepo = require("../../state/stateRepository");
+const input = require("../../interface/templates/inputTemplate");
 
 module.exports = (user, text, state) => {
 
@@ -7,5 +8,5 @@ module.exports = (user, text, state) => {
   state.step = "model";
   stateRepo.set(user, state);
 
-  return send(user, "Enter Model:");
+  return send(user, input.model());
 };
