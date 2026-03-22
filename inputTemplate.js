3363f82822c345482384f581e72394fa214064dd
imkhan89 <info@ndestore.com>
2026-03-22 19:50:36 +0500

    Create inputTemplate.js

---
+++ inputTemplate.js
@@ -0,0 +1,7 @@
+module.exports = {
+  make: () => "Enter Make:",
+  model: () => "Enter Model:",
+  year: () => "Enter Year:",
+  name: () => "Enter Your Name:",
+  address: () => "Enter Address:"
+};
