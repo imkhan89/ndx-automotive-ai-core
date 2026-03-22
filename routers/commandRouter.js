a4393cd20a62b6a7b7f6efa48b383e6c77f4606b
imkhan89 <info@ndestore.com>
2026-03-22 22:15:48 +0500

    Delete routers/commandRouter.js

--- routers/commandRouter.js
+++
@@ -1,9 +0,0 @@
-const menuCommand = require("../commands/menuCommand");
-
-exports.route = (text) => {
-  text = text.toLowerCase();
-
-  if (text === "menu") return menuCommand;
-
-  return null;
-};
