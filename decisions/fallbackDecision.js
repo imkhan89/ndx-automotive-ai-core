c8531c57b7380f3d038ea20b6d95ec0a9aa775ec
imkhan89 <info@ndestore.com>
2026-03-22 20:51:58 +0500

    Update fallbackDecision.js

--- decisions/fallbackDecision.js
+++ decisions/fallbackDecision.js
@@ -2,7 +2,8 @@ module.exports = (products) => {
 
   if (!products || products.length === 0) {
     return {
-      type: "no_results"
+      type: "no_results",
+      message: "No products found. Try another option or type support."
     };
   }
 
