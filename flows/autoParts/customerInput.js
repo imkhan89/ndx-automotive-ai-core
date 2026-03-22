0ac26f825ec2a13d09589b5a8982f7e10a847968
imkhan89 <info@ndestore.com>
2026-03-22 20:22:40 +0500

    Update customerInput.js

--- flows/autoParts/customerInput.js
+++ flows/autoParts/customerInput.js
@@ -1,17 +1,23 @@
 const send = require("../../services/whatsappService").send;
 const stateRepo = require("../../state/stateRepository");
 
+const inputTemplate = require("../../interface/templates/inputTemplate");
+
 module.exports = (user, text, state) => {
 
+  // First input → Name
   if (!state.customer) {
     state.customer = {};
     state.customer.name = text;
+
     stateRepo.set(user, state);
 
-    return send(user, "Enter Address:");
+    return send(user, inputTemplate.address());
   }
 
+  // Second input → Address
   state.customer.address = text;
+
   state.step = "create";
   stateRepo.set(user, state);
 
